809a898bcaa2489d9b85b84bbb35f1d04f2244fb
alpsakaci <alpsakaci@protonmail.com>
2022-01-08 23:05:29 +0300

    init project

---
+++ src/main/kotlin/com/alpsakaci/listflit/ListFlitApplication.kt
@@ -0,0 +1,11 @@
+package com.alpsakaci.listflit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ListFlitApplication
+
+fun main(args: Array<String>) {
+	runApplication<ListFlitApplication>(*args)
+}
---
+++ src/test/kotlin/com/alpsakaci/listflit/ListFlitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alpsakaci.listflit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ListFlitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
